ca2747bcb6912646b57f3b66c614c855769aec29
Marcus D. Hanwell <mhanwell@bnl.gov>
2022-12-20 13:51:15 -0500

    First toy prototype submitting a plan over zmq
    This adds a button and uses zmq to submit a plan to a prototype
    RE-lite service on the localhost. The UID matches on both sides in a
    simulated environment.

--- tomoco/app.py
+++ tomoco/app.py
@@ -1,14 +1,60 @@
 import sys
 
+import json
+import zmq
+
 from qtpy.QtWidgets import (
     QApplication,
     QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout,
+    QWidget,
 )
 
 class App(QMainWindow):
     def __init__(self, parent=None):
         super(App, self).__init__(parent)
-        self.setWindowTitle("Tomoco: Tomography Data Acquisition")
+        self.setWindowTitle('Tomoco: Tomography Data Acquisition')
+        self.button = QPushButton('Go!')
+        layout = QVBoxLayout()
+        hButtonBox = QHBoxLayout()
+        hButtonBox.addStretch()
+        hButtonBox.addWidget(self.button)
+        hButtonBox.addStretch()
+        layout.addLayout(hButtonBox)
+
+        # Set main windows widget using our central layout
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        # Add button signal to slot to start/stop
+        self.button.clicked.connect(self.buttonPressed)
+
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PAIR)
+        self.socket.connect('tcp://127.0.0.1:5556')
+
+    def buttonPressed(self):
+        # Currently being a little lame - only update state on start/stop.
+        print('Button pressed!', self.button.text())
+        if self.button.text() == 'Go':
+            self.button.setText('Stop')
+        else:
+            self.button.setText('Go')
+        
+        self.run("scan", "([det], motor, 1, 5, 5)", "{}")
+
+    def run(self, plan_name, args, kwargs):
+        to_send = json.dumps({"plan_name": plan_name, "plan_args": args, "plan_kwargs": kwargs}).encode()
+        self.socket.send(to_send)
+        print('sent', to_send)
+        msg = self.socket.recv()
+        if msg:
+            decoded_msg = json.loads(msg.decode())
+            print('received', decoded_msg)
+
 
 def run():
     # Set up some application basics for saving settings
